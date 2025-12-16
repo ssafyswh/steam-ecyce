2e44bc5887ceea8862577e7bf272e220df5cb437
문현아 <gusdk9719280@gmail.com>
2025-12-17 00:24:40 +0900

    [feat] Login with Steam
    Steam 아이디로 로그인하기 구현

--- FrontEnd/src/router/index.js
+++ FrontEnd/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue' 
+import SteamCallback from '../views/SteamCallback.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,6 +19,16 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/auth/callback',
+      name: 'SteamCallback',
+      component: SteamCallback
+    }
   ],
 })
 
