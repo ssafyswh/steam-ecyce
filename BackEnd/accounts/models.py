1bb27860244f586189f0b75935ce7086f4114d83
ssafyswh <ssafyswh@gmail.com>
2025-12-19 14:55:56 +0900

    [feat] Edit accounts/models.py
    사용자 정보 갱신 시점을 저장하는 last_synced_at 필드 추가
    과도한 api 호출 방지 용도로 사용 가능함

--- BackEnd/accounts/models.py
+++ BackEnd/accounts/models.py
@@ -3,10 +3,13 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     # 스팀 닉네임 (스팀 id는 username에 저장되어있음)
-    nickname = models.CharField(max_length=100, blank=True, null=True)
+    nickname = models.CharField(max_length=100, blank=True, default="")
     
     # 스팀 프로필 사진 URL (avatarfull)
-    avatar = models.URLField(blank=True, null=True)
+    avatar = models.URLField(max_length=500, blank=True, null=True)
+
+    # 사용자 라이브러리 정보 갱신된 가장 최근 시점
+    last_synced_at = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.nickname if self.nickname else self.username
